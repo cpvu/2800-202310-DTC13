7f4ac2697eda055754ecf2195504f4b50031875b
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-08 15:23:59 -0700

    Add env file to gitignore

--- server/server.js
+++ server/server.js
@@ -1,17 +1,45 @@
 import bodyParser from "body-parser";
-import express from "express"; 
+import express from "express";
 import { apiRouter } from "./routes/api.router";
+import * as dotenv from "dotenv";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import sessions from "express-sessions";
+
+dotenv.config();
 
 export const expressServer = () => {
     const app = express();
 
-    //mongo
+    try {
+        mongoose.connect(process.env, {
+            dbName: "CryptomentAI",
+            keepAlive: true
+        });
 
-    app.use(json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json()); 
+        app.use(json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+        app.use(cors());
 
-    app.use("/api", apiRouter()); 
+        app.use(sessions({
+            saveUninitialized: true,
+            authenticated: Boolean,
+            username: String,
+            cookie: { maxAge: 0 },
+            resave: false,
+            store: MongoStore.create({
+                mongoUrl: process.env.MONGO_URL,
+            }),
+        })
+        );
 
-    return app;
+        //add root routes here and router
+        app.use("/api", apiRouter());
+
+        return app;
+
+    } catch (err) {
+        console.log(err);
+    }
 }
