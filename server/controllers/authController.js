bace7e443bfefec9b8fe285b6930f9e1f6734b02
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-08 15:24:12 -0700

    Bootstrap express server

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,9 @@
 export const postLogin = () => {
+    //add login logic here
+    return;
+}
+
+export const postSignup = () => {
+    //add signup logic here
     return;
 }
--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 export const apiRouter = () => {
     const router = Router(); 
     
+    //Add routes here for API call
     router.post("/login", postLogin); 
+    router.post("/signup", postSignup); 
 
     return router;
 }
