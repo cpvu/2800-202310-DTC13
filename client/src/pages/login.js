1ed1942e3f90fb3a30918a9ff07bf60729027ed8
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-09 16:03:40 -0700

    Add login page components

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,20 @@
 import LoginForm from "@/components/LoginForm";
+import { Navbar } from "@/components/Navbar";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Box
+      w={"100%"}
+      maxW={"lg"}
+      minH={"100vh"}
+      display="flex"
+      flexDirection="column"
+    >
+      {isDesktop ? <Navbar /> : <></>}
+      <LoginForm></LoginForm>
+      {isDesktop ? <></> : <Navbar style={"align"}></Navbar>}
+    </Box>
+  );
 }
