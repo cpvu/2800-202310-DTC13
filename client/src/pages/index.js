387af055963a6eddeb70ee4191e311332936f50c
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-09 23:01:12 -0700

    Add home header to landing page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,12 +4,14 @@ import { Inter } from "next/font/google";
 import { Box, Heading } from "@chakra-ui/react";
 import styles from "@/styles/Home.module.css";
 import { Navbar, HomeHeader } from "@/components";
+import SignUp from "./signup";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <HomeHeader></HomeHeader>
       <HomeHeader></HomeHeader>
     </>
   );
