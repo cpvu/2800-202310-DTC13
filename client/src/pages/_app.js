d56dba2329d900d1b7612f688f28d55fb2d6fc5e
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-09 23:00:55 -0700

    Add custom theme with scroll behavior

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,21 @@
-//import '@/styles/globals.css'
-import { ChakraProvider } from "@chakra-ui/react";
+import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Layout from "../components/Layout";
+
+const customTheme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        overscrollBehavior: "none",
+        scrollBehavior: "smooth",
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
