ec89faf4a46dae142d841aa195ed0190d3542514
Bryan Fung <bryan_fung@yahoo.com>
2023-05-10 10:08:54 -0700

    Fix mongoose model

--- models/authSchema.js
+++ models/authSchema.js
@@ -1,23 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String
 });
 
 const User = mongoose.model('User', userSchema);
