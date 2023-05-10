141daccdd4513253fe7bf5daea29be58f854611d
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-09 22:02:50 -0700

    Add layout to project app

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,13 @@
 //import '@/styles/globals.css'
 import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
