dcde877c79a339fea2bd5290fd87f079bde6dd6b
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-10 01:18:05 -0700

    Refactor imports and model folder

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,40 +1,44 @@
-import User from '../../models/authSchema.js';
-import express from 'express';
-import session from 'express-session';
-import bcrypt from 'bcrypt';
-import Joi from 'joi';
+import { User } from "../models/authSchema";
+import express from "express";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import Joi from "joi";
 
 const saltRounds = 12;
 
 export const postLogin = async (req, res) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
   });
 
   const { error, value } = schema.validate(req.body);
   if (error) {
-    return res.status(400).json({ message: `Please provide ${error.details[0].message}.` });
+    return res
+      .status(400)
+      .json({ message: `Please provide ${error.details[0].message}.` });
   }
 
   try {
     const user = await User.findOne({ email: value.email });
     if (!user) {
-      return res.status(404).json({ message: `No user with email ${value.email}.` });
+      return res
+        .status(404)
+        .json({ message: `No user with email ${value.email}.` });
     }
     const match = await bcrypt.compare(value.password, user.password);
     if (!match) {
-      return res.status(401).json({ message: 'Incorrect password.' });
+      return res.status(401).json({ message: "Incorrect password." });
     }
     req.session.user = {
       _id: user._id,
       firstName: user.firstName,
-      email: user.email
+      email: user.email,
     };
-    res.status(200).json({ message: 'Login successful.' });
+    res.status(200).json({ message: "Login successful." });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error occurred during login.' });
+    res.status(500).json({ message: "Error occurred during login." });
   }
 };
 
@@ -43,18 +47,22 @@ export const postSignup = async (req, res) => {
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
   });
 
   try {
     const { error, value } = await schema.validateAsync(req.body);
     if (error) {
-      return res.status(400).json({ error: `Please provide ${error.details[0].message}.` });
+      return res
+        .status(400)
+        .json({ error: `Please provide ${error.details[0].message}.` });
     }
 
     const existingUser = await User.findOne({ email: value.email });
     if (existingUser) {
-      return res.status(400).json({ error: `User with email ${value.email} already exists.` });
+      return res
+        .status(400)
+        .json({ error: `User with email ${value.email} already exists.` });
     }
 
     const hashedPassword = await bcrypt.hash(value.password, saltRounds);
@@ -63,7 +71,7 @@ export const postSignup = async (req, res) => {
       firstName: value.firstName,
       lastName: value.lastName,
       email: value.email,
-      password: hashedPassword
+      password: hashedPassword,
     });
 
     await user.save();
@@ -72,12 +80,14 @@ export const postSignup = async (req, res) => {
       _id: user._id,
       firstName: user.firstName,
       lastName: user.lastName,
-      email: user.email
+      email: user.email,
     };
 
-    return res.status(200).json({ message: 'Signup successful.' });
+    return res.status(200).json({ message: "Signup successful." });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Error occurred during signup. Please try again.' });
+    return res
+      .status(500)
+      .json({ error: "Error occurred during signup. Please try again." });
   }
 };
--- server/models/authSchema.js
+++ server/models/authSchema.js
@@ -1,25 +1,23 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true
+    required: true,
   },
   lastName: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+export const User = mongoose.model("User", userSchema);
