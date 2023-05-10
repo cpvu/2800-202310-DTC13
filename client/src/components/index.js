2629fadfa741c8071a85061afe9acea08eef53d0
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-09 22:03:40 -0700

    Add index.js for imports

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./HomeHeader";
+import LoginForm from "./LoginForm";
+import Navbar from "./Navbar";
+import SignUpForm from "./SignUpForm";
+
+export { HomeHeader, LoginForm, Navbar, SignUpForm };
