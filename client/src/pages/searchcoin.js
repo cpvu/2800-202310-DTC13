eb9603acabc1e6e0ed6786c6e92b193bab6e2d19
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-11 01:08:49 -0700

    Add temp user search page

---
+++ client/src/pages/searchcoin.js
@@ -0,0 +1,11 @@
+export default function SearchCoin() {
+    return (
+        <>
+        <h1>Welcome User!</h1>
+
+        <p>Search a coin to get started!</p>
+        <input type="input"></input>
+        </>
+        
+    )
+}
