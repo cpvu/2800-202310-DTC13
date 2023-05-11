b9b4a9f10e9ee8ba9c1561017b2c48ec0e665257
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-11 10:00:53 -0700

    Refactor signup and login forms with username

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -11,54 +11,50 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  ButtonSpinner
+  ButtonSpinner,
 } from "@chakra-ui/react";
-import React, { useRef } from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import React, { useRef } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import Router, { useRouter } from "next/router";
 
 export default function LoginForm() {
-
   const router = useRouter();
 
   const initialValues = {
-    email: '',
-    password: '',
+    username: "",
+    password: "",
   };
 
   async function handleLogin(values, { setSubmitting }) {
     setTimeout(async () => {
-
       const payload = {
-       "email": values.email,
-       "password": values.password
-      }
+        email: values.username,
+        password: values.password,
+      };
 
       const options = {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(payload),
       };
 
       try {
         let response = await fetch("http://localhost:8000/api/login", options);
-        let responseJSON = await response.json()
+        let responseJSON = await response.json();
 
         if (responseJSON.authenticated) {
-          router.push("/searchcoin")
+          router.push("/searchcoin");
         }
-        
-        console.log(responseJSON)
 
+        console.log(responseJSON);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-     
-      setSubmitting(false)
-    }, 1000)
 
+      setSubmitting(false);
+    }, 1000);
   }
 
   return (
@@ -70,11 +66,21 @@ export default function LoginForm() {
       flex={"1"}
       bg={useColorModeValue("gray.50", "gray.800")}
     >
-      <Stack  align={"center"} spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+      <Stack
+        align={"center"}
+        spacing={8}
+        mx={"auto"}
+        maxW={"lg"}
+        py={12}
+        px={6}
+      >
         <Stack>
           <Heading fontSize={"4xl"}>Log in to your account</Heading>
           <Text fontSize={"lg"} color={"gray.600"} align={"center"}>
-            Not a user? Sign up <Link color={"blue.400"} href="./signup">here</Link> 
+            Not a user? Sign up{" "}
+            <Link color={"blue.400"} href="./signup">
+              here
+            </Link>
           </Text>
         </Stack>
         <Box
@@ -87,39 +93,49 @@ export default function LoginForm() {
         >
           <Stack spacing={4}>
             <Formik initialValues={initialValues} onSubmit={handleLogin}>
-              {({isSubmitting, values, handleChange, handleBlur}) => (
-              <Form>
-                <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
-                  <Input type="email" name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} />
-                </FormControl>
-                <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
-                  <Input type="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password}  />
-                </FormControl>
-                <Stack spacing={10}>
-                  <Stack
-                    direction={{ base: "column", sm: "row" }}
-                    align={"start"}
-                    justify={"space-between"}
-                  >
-                    <Checkbox>Remember me</Checkbox>
-                    <Link color={"blue.400"}>Forgot password?</Link>
+              {({ isSubmitting, values, handleChange, handleBlur }) => (
+                <Form>
+                  <FormControl id="username" isRequired>
+                    <FormLabel>Username</FormLabel>
+                    <Input
+                      type="text"
+                      name="username"
+                      onChange={handleChange}
+                      value={values.username}
+                    />
+                  </FormControl>
+                  <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                    />
+                  </FormControl>
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: "column", sm: "row" }}
+                      align={"start"}
+                      justify={"space-between"}
+                    >
+                      <Checkbox>Remember me</Checkbox>
+                      <Link color={"blue.400"}>Forgot password?</Link>
+                    </Stack>
+                    <Button
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? <ButtonSpinner /> : "Sign In"}
+                    </Button>
                   </Stack>
-                  <Button
-                    bg={"blue.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "blue.500",
-                    }}
-
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                     {isSubmitting ? <ButtonSpinner /> : 'Sign In'}
-                  </Button>
-                </Stack>
-              </Form>
+                </Form>
               )}
             </Formik>
           </Stack>
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -16,7 +16,7 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Formik,Form, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from "formik";
 import Router, { useRouter } from "next/router";
 
 export default function SignUpForm() {
@@ -24,49 +24,46 @@ export default function SignUpForm() {
   const [showPassword, setShowPassword] = useState(false);
 
   const initialValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
   };
 
   async function handleLogin(values, { setSubmitting, setErrors }) {
-
     setTimeout(async () => {
-    const payload = {
+      const payload = {
+        username: values.username,
         firstName: values.firstName,
         lastName: values.lastName,
         email: values.email,
         password: values.password,
-      }
+      };
 
       const options = {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(values)
+        body: JSON.stringify(values),
       };
 
       try {
         let response = await fetch("http://localhost:8000/api/signup", options);
+        let responseJSON = await response.json();
 
-        let responseJSON = await response.json()
-
-        if (response.status == 200) {
-          router.push("/login")
-        }
+        router.push("/login");
       } catch (err) {
-        setErrors({ server: error.message });
-        console.log(err)
+        //setErrors({ server: error.message });
+        console.log(err);
       }
-    })
-  
-      setSubmitting(false)
+    });
+
+    setSubmitting(false);
   }
 
   return (
-
     <Flex
       py={{ base: "5", md: "auto" }}
       pb={{ md: "150px" }}
@@ -92,54 +89,85 @@ export default function SignUpForm() {
           p={8}
         >
           <Formik initialValues={initialValues} onSubmit={handleLogin}>
-          {({isSubmitting, values, handleChange, handleBlur}) => (
-            <Form>
-            <Stack spacing={4}>
-              <FormControl id="firstName" isRequired>
-                <FormLabel>First Name</FormLabel>
-                <Input type="text" name="firstName" onChange={handleChange} value={values.firstName} />
-              </FormControl>
-              <FormControl id="lastName">
-                <FormLabel>Last Name</FormLabel>
-                <Input type="text" name="lastName" onChange={handleChange} value={values.lastName}  />
-              </FormControl>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" name="email" onChange={handleChange} value={values.email} />
-              </FormControl>
-              <FormControl id="password" name="password" onChange={handleChange} value={values.password} isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword ? "text" : "password"} />
-                  <InputRightElement h={"full"}>
-                    <Button
-                      variant={"ghost"}
-                      onClick={() =>
-                        setShowPassword((showPassword) => !showPassword)
-                      }
-                    >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                type="submit"
-              >
-                Sign up
-              </Button>
-              <Text align={"center"}>
-                Already a user? <Link color={"blue.400"}>Login</Link>
-              </Text>
-            </Stack>
-            </Form>   )}
+            {({ isSubmitting, values, handleChange, handleBlur }) => (
+              <Form>
+                <Stack spacing={4}>
+                  <FormControl id="username" isRequired>
+                    <FormLabel>Username</FormLabel>
+                    <Input
+                      type="text"
+                      name="username"
+                      onChange={handleChange}
+                      value={values.username}
+                    />
+                  </FormControl>
+                  <FormControl id="firstName" isRequired>
+                    <FormLabel>First Name</FormLabel>
+                    <Input
+                      type="text"
+                      name="firstName"
+                      onChange={handleChange}
+                      value={values.firstName}
+                    />
+                  </FormControl>
+                  <FormControl id="lastName">
+                    <FormLabel>Last Name</FormLabel>
+                    <Input
+                      type="text"
+                      name="lastName"
+                      onChange={handleChange}
+                      value={values.lastName}
+                    />
+                  </FormControl>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      value={values.email}
+                    />
+                  </FormControl>
+                  <FormControl
+                    id="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={values.password}
+                    isRequired
+                  >
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input type={showPassword ? "text" : "password"} />
+                      <InputRightElement h={"full"}>
+                        <Button
+                          variant={"ghost"}
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        >
+                          {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                  <Button
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={"blue.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}
+                    type="submit"
+                  >
+                    Sign up
+                  </Button>
+                  <Text align={"center"}>
+                    Already a user? <Link color={"blue.400"}>Login</Link>
+                  </Text>
+                </Stack>
+              </Form>
+            )}
           </Formik>
         </Box>
       </Stack>
