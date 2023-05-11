f0bf554543872bdaf7b02d9dd9229e6b10bef490
Bryan Fung <bryan_fung@yahoo.com>
2023-05-11 11:20:49 -0700

    Update login and signup feature to allow users to login with their username instead of email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,42 +6,6 @@ import Joi from "joi";
 
 const saltRounds = 12;
 
-export const postLogin = async (req, res) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  });
-
-  const { error, value } = schema.validate(req.body);
-  if (error) {
-    return res
-      .status(400)
-      .json({ message: `Please provide ${error.details[0].message}.` });
-  }
-
-  try {
-    const user = await User.findOne({ email: value.email });
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: `No user with email ${value.email}.` });
-    }
-    const match = await bcrypt.compare(value.password, user.password);
-    if (!match) {
-      return res.status(401).json({ message: "Incorrect password." });
-    }
-    req.session.user = {
-      _id: user._id,
-      firstName: user.firstName,
-      email: user.email,
-    };
-    res.status(200).json({ message: "Login successful." });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error occurred during login." });
-  }
-};
-
 export const postSignup = async (req, res) => {
   const schema = Joi.object({
     username: Joi.string().required(),
