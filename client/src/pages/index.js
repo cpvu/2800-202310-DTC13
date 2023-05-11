e2a7f4f169ce0daae7a186ad729225823e70075d
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-11 10:55:40 -0700

    Fix horizontal scroll bug for larger viewports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Box, Heading } from "@chakra-ui/react";
-import styles from "@/styles/Home.module.css";
+//import styles from "@/styles/Home.module.css";
 import { Navbar, HomeHeader } from "@/components";
 import SignUp from "./signup";
 
