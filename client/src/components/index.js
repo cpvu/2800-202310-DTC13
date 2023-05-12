00e54a2cbf4783147169ddb2aa62e4c733801ee8
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-11 23:30:08 -0700

    Add import/export

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,6 @@ import LoginForm from "./LoginForm";
 import Navbar from "./Navbar";
 import SignUpForm from "./SignUpForm";
 import ForgotPasswordForm from "./ForgotPasswordForm";
+import ResetPasswordForm from "./ResetPasswordForm";
 
-export { HomeHeader, LoginForm, Navbar, SignUpForm, ForgotPasswordForm };
+export { HomeHeader, LoginForm, Navbar, SignUpForm, ForgotPasswordForm, ResetPasswordForm };
