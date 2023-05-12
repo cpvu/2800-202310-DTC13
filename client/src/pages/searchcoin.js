04320ba533282e9283d9431232482d3415a395b8
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-12 02:00:22 -0700

    Add client session check

--- client/src/pages/searchcoin.js
+++ client/src/pages/searchcoin.js
@@ -1,22 +1,21 @@
 import { useSession, getSession } from "next-auth/react"
+import { Router, useRouter } from "next/router";
 
 export default function SearchCoin() {
+    const router = useRouter()
     const { data: session } = useSession()
     return (
         <>
-        <h1>Welcome User!</h1>
+        {session ? <> <h1>Welcome User!</h1>
         <p>Search a coin to get started!</p>
         <input type="input"></input>
-        <h1>{session.user.name}</h1>
+        <h1>{session.user.name}</h1>   </>: <h1>Sign up or login to get access!</h1>}
         </>
-        
     )
 }
 
 export async function getServerSideProps(context) {
     const session = await getSession(context);
-    console.log(session)
-    // Pass the session object as a prop to your page component
     return {
       props: {
         session: session,
