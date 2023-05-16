1af7a24a7bc0933a3bc41e241d605f625a8a86e7
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-16 12:45:31 -0700

    Add custom breakpoints for theme

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,9 +1,16 @@
 //import "@/styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Layout from "../components/Layout";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
 const customTheme = extendTheme({
+  breakpoints: {
+    xs: "22em",
+    sm: "32em",
+    md: "48em",
+    lg: "62em",
+    xl: "80em",
+  },
   styles: {
     global: {
       "html, body": {
@@ -14,12 +21,15 @@ const customTheme = extendTheme({
   },
 });
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider theme={customTheme}>
         <Layout>
-          <Component {...pageProps}  />
+          <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
     </SessionProvider>
