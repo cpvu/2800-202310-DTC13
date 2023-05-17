24edf27a17eb75f20b8513f0d066ab6038065080
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-17 14:39:05 -0700

    Add market coin info component

--- client/src/pages/search/[...coin].js
+++ client/src/pages/search/[...coin].js
@@ -1,7 +1,16 @@
 import { useSession } from "next-auth/react";
 import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Stack, Container, Heading } from "@chakra-ui/react";
+import {
+  Stack,
+  Container,
+  Heading,
+  HStack,
+  Box,
+  Center,
+  Divider,
+} from "@chakra-ui/react";
+import TokenPageDivider from "@/components/coin/TokenPageDivider";
 
 export default function CryptocurrencyCoinPage() {
   const router = useRouter();
@@ -10,10 +19,30 @@ export default function CryptocurrencyCoinPage() {
   let { token } = router.query;
 
   return (
-    <Container m={0} p={5}>
-      <Stack align={"left"} pt={"10px"}>
-        {session ? <Heading>{token}</Heading> : <p></p>}
-      </Stack>
+    <Container
+      border={"1px"}
+      borderColor={"gray.300"}
+      mx={"auto"}
+      mt={"60px"}
+      minH={"100%"}
+      width={"85%"}
+      maxW={"85%"}
+      boxShadow={"0px 2px 4px rgba(0, 0, 0, 0.1)"}
+    >
+      <HStack
+        border={"1px"}
+        borderColor={"gray.300"}
+        width={"100%"}
+        maxWidth={"100%"}
+        my={"25px"}
+        p={"20px"}
+        overflow={"hidden"}
+      >
+        <Box>{session ? <Heading>{token}</Heading> : <p></p>}</Box>
+        <TokenPageDivider />
+        <Box>{session ? <Heading>Price</Heading> : <p></p>}</Box>
+        <TokenPageDivider />
+      </HStack>
     </Container>
   );
 }
