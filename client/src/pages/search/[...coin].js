ad390365a49f7f8766b5e893b94ac10b405242a1
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-17 09:29:13 -0700

    Bootstrap dynamic coin page

--- client/src/pages/search/[...coin].js
+++ client/src/pages/search/[...coin].js
@@ -1,3 +1,19 @@
+import { useSession } from "next-auth/react";
+import Router, { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Stack, Container, Heading } from "@chakra-ui/react";
+
 export default function CryptocurrencyCoinPage() {
-  return <h1>ETH</h1>;
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  let { token } = router.query;
+
+  return (
+    <Container m={0} p={5}>
+      <Stack align={"left"} pt={"10px"}>
+        {session ? <Heading>{token}</Heading> : <p></p>}
+      </Stack>
+    </Container>
+  );
 }
