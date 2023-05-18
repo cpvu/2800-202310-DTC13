4c86f222365b986ed5998bdd9257aaf0dddc9b7f
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-18 09:32:35 -0700

    Add lazy loading for nav

--- client/src/components/layouts/Layout.js
+++ client/src/components/layouts/Layout.js
@@ -1,48 +1,36 @@
-import Navbar from "../common/Navbar";
-import { Container, Flex, Box, useMediaQuery } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import Footer from "../common/Footer";
-import BottomNavbar from "../common/BottomNavbar";
+import { lazy, Suspense, useEffect, useState } from 'react';
+import { Container, Flex, Box, useMediaQuery } from '@chakra-ui/react';
+import Footer from '../common/Footer';
+
+const Navbar = lazy(() => import('../common/Navbar'));
+const BottomNavbar = lazy(() => import('../common/BottomNavbar'));
 
 export default function Layout({ children }) {
-  const [isDesktop, setIsDesktop] = useState(false);
+  const [isDesktop] = useMediaQuery('(min-width: 768px)');
   const [shouldRenderBottomNavbar, setShouldRenderBottomNavbar] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(min-width: 768px)");
-
-    const handleResize = () => {
-      setIsDesktop(mediaQuery.matches);
-      setShouldRenderBottomNavbar(!mediaQuery.matches);
-    };
-
-    handleResize(); // Set the initial value
-
-    // Add event listener for resize
-    mediaQuery.addEventListener("change", handleResize);
-
-    // Clean up the event listener
-    return () => {
-      mediaQuery.removeEventListener("change", handleResize);
-    };
-  }, []);
+    setShouldRenderBottomNavbar(!isDesktop);
+  }, [isDesktop]);
 
   return (
-    <>
-      <Flex
-        minW={"100%"} 
-        minH={"100vh"}
-        display="flex"
-        flexDirection="column"
-        overflow="hidden"
-        p={0}
-      >
-        {isDesktop && <Navbar />}
-        {children}
-        <Container id="root" flex={"1"}></Container>
-        <Footer></Footer>
-        {shouldRenderBottomNavbar && <BottomNavbar />}
-      </Flex>
-    </>
+    <Flex
+      minW={'100%'}
+      minH={'100vh'}
+      display="flex"
+      flexDirection="column"
+      overflow="hidden"
+      p={0}
+    >
+      {isDesktop && <Navbar />}
+      {children}
+      <Container id="root" flex={'1'}></Container>
+      <Footer></Footer>
+      {shouldRenderBottomNavbar && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <BottomNavbar />
+        </Suspense>
+      )}
+    </Flex>
   );
 }
