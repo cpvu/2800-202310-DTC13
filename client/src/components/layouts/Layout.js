d44287e9dd2085d89e10c7899736fb9f5ffc3a8e
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-17 23:01:46 -0700

    Fix bug

---
+++ client/src/components/layouts/Layout.js
@@ -0,0 +1,40 @@
+import Navbar from "../common/Navbar";
+import { Container, Flex, useColorModeValue } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Footer from "../common/Footer";
+
+export default function Layout({ children }) {
+  const [isDesktop, setIsDesktop] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1024); // Set the desired breakpoint for desktop
+    };
+
+    handleResize(); // Set initial value
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <Flex
+        minH={"100vh"}
+        display="flex"
+        flexDirection="column"
+        overflow="hidden"
+        bg={useColorModeValue("white.50", "gray.800")}
+      >
+        {isDesktop && <Navbar />}
+        {children}
+        <Container flex={"1"}></Container>
+        <Footer></Footer>
+        {!isDesktop && <Navbar />}
+      </Flex>
+    </>
+  );
+}
