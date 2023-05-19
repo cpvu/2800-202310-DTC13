64e3dcdca0a7a49f492787ff5f45b4eedad31e13
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-19 04:09:11 -0700

    Refactor screen size for mobile for divider

--- client/src/components/coin/TokenPageDivider.js
+++ client/src/components/coin/TokenPageDivider.js
@@ -1,9 +1,11 @@
-import { Center, Divider } from "@chakra-ui/react";
+import { Center, Divider, useMediaQuery } from "@chakra-ui/react";
 
 export default function TokenPageDivider() {
+  const [isSmScreen] = useMediaQuery("(max-width: 640px)");
   return (
-    <Center height="50px">
+    <>    {isSmScreen ? <Center height="50px">
       <Divider m={"10px"} orientation="vertical" borderColor="gray.500" />
-    </Center>
+    </Center> : <></>} </>
+
   );
 }
