d4e905213ec07101ca63898beff78360e2f8f794
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-18 23:27:30 -0700

    Add coin information endpoint to constants

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -2,3 +2,5 @@ export const FORGOT_PASSWORD_ENDPOINT = "/api/forgotPassword";
 export const RESET_PASSWORD_ENDPOINT = "/api/resetPassword/";
 export const SIGN_UP_ENDPOINT = "/api/signup/";
 export const LOGIN_ENDPOINT = "/api/login/";
+
+export const COIN_INFORMATION = "/api/getCoinInformation"
