8d508255c18952cb14d1d7284b4791acd74de6c3
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-20 16:55:05 -0700

    Refactor snake game to clicking on home page i

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,26 +1,7 @@
 import { Container, Stack, ButtonGroup, IconButton, Text } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import SnakeMiniGame from "../home/SnakeMiniGame";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  const [easterEggCount, setEasterEggCount] = useState(0);
-  let clickTimeout;
-
-  useEffect(() => {
-    if (easterEggCount === 5) {
-      SnakeMiniGame();
-    }
-  }, [easterEggCount]);
-
-  const handleTextClick = () => {
-    clearTimeout(clickTimeout);
-    setEasterEggCount(easterEggCount + 1);
-
-    clickTimeout = setTimeout(() => {
-      setEasterEggCount(0);
-    }, 3000);
-  };
 
   return (
     <Container as="footer" role="contentinfo" pt={{ base: "12", md: "16" }} pb={{ base: "4", md: "6" }} align="center">
@@ -32,7 +13,7 @@ export default function Footer() {
             <IconButton as="a" href="#" aria-label="Twitter" />
           </ButtonGroup>
         </Stack>
-        <Text fontSize="sm" color="subtle" onClick={handleTextClick}>
+        <Text fontSize="sm" color="subtle">
           &copy; {currentYear} by Ankit Ahlwat, Bryan Fung, Calvin Vu, and Micheal Yu
         </Text>
       </Stack>
