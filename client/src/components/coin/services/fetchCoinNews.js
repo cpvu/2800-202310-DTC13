eb2e51a7855c6fa78915b4ec7dc4d584f5e44feb
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 00:24:11 -0700

    Create coin news api call

---
+++ client/src/components/coin/services/fetchCoinNews.js
@@ -0,0 +1,14 @@
+export default async function fetchCoinNews(coin) {
+    const options = {
+        "method": "GET", 
+        "Content-header": "application/json"
+    }
+    try {
+        let response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_22499c45bb1fac4bfbd4adfb4a135e94affbe&qInTitle=${coin}&language=en`, options);
+        let responseJSON = await response.json();
+        
+        return responseJSON.results.slice(0,5);
+    } catch (e) {
+        console.log(e)
+    }
+}
