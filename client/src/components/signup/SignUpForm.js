ce1b06663750ba24992b7e68576df85c04071e97
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 15:41:54 -0700

    Add signup form error handling

--- client/src/components/signup/SignUpForm.js
+++ client/src/components/signup/SignUpForm.js
@@ -9,19 +9,23 @@ import {
   InputRightElement,
   Stack,
   Button,
+  FormErrorMessage,
   Heading,
   Text,
   useColorModeValue,
   Link,
+  useToast
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Formik, Form, ErrorMessage } from "formik";
 import { useRouter } from "next/router";
+import { SignupSchema } from "./SignupSchema";
 import { SIGN_UP_ENDPOINT } from "@/constants/endpoints";
 
 export default function SignUpForm() {
   const router = useRouter();
+  const toast = useToast();
   const [showPassword, setShowPassword] = useState(false);
 
   const initialValues = {
@@ -47,12 +51,35 @@ export default function SignUpForm() {
       try {
         let response = await fetch(baseURL + SIGN_UP_ENDPOINT, options);
         let responseJSON = await response.json();
+
+        console.log(responseJSON)
+
+        if (!responseJSON.success) {
+          throw new Error(responseJSON.message)
+        }
+
+        toast({
+          title: "Succesfully signed up! ",
+          description: `Redirecting you to the login page`,
+          position: "top",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+
         router.push("/login");
+
       } catch (err) {
-        //setErrors({ server: error.message });
+        toast({
+          title: "Sign up Error",
+          description: `${err}`,
+          position: "top",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
         console.log(err);
       }
-      alert("Successfully signed up!");
     });
     setSubmitting(false);
   }
@@ -77,58 +104,73 @@ export default function SignUpForm() {
         <Box
           rounded={"lg"}
           minW={"20vw"}
+          w={"100%"}
           bg={useColorModeValue("white", "gray.700")}
           boxShadow={"lg"}
           p={8}
+          overflow="hidden"
         >
-          <Formik initialValues={initialValues} onSubmit={handleSignUp}>
-            {({ isSubmitting, values, handleChange, handleBlur }) => (
+          <Formik initialValues={initialValues} validationSchema={SignupSchema} onSubmit={handleSignUp}>
+            {({ isSubmitting, values, handleChange, handleBlur, errors, touched }) => (
               <Form>
                 <Stack spacing={4}>
-                  <FormControl id="username" isRequired>
+                  <FormControl id="username" isRequired isInvalid={!!errors.username && touched.username}>
                     <FormLabel>Username</FormLabel>
                     <Input
                       type="text"
                       name="username"
                       onChange={handleChange}
                       value={values.username}
+                      onBlur={handleBlur}
                     />
+                    <FormErrorMessage fontSize={"0.73em"}>{errors.username}</FormErrorMessage>
                   </FormControl>
-                  <FormControl id="firstName" isRequired>
+                  <FormControl id="firstName" isRequired isInvalid={!!errors.firstName && touched.firstName}>
                     <FormLabel>First Name</FormLabel>
                     <Input
                       type="text"
                       name="firstName"
                       onChange={handleChange}
                       value={values.firstName}
+                      onBlur={handleBlur}
                     />
+                    <FormErrorMessage fontSize={"0.73em"}>{errors.firstName}</FormErrorMessage>
                   </FormControl>
-                  <FormControl id="lastName">
+                  <FormControl id="lastName" isRequired isInvalid={!!errors.lastName && touched.lastName}>
                     <FormLabel>Last Name</FormLabel>
                     <Input
                       type="text"
                       name="lastName"
                       onChange={handleChange}
                       value={values.lastName}
+                      onBlur={handleBlur}
+
                     />
+                    <FormErrorMessage fontSize={"0.73em"}>{errors.lastName}</FormErrorMessage>
                   </FormControl>
-                  <FormControl id="email" isRequired>
+                  <FormControl id="email" isRequired isInvalid={!!errors.email && touched.email}>
                     <FormLabel>Email address</FormLabel>
                     <Input
                       type="email"
                       name="email"
                       onChange={handleChange}
                       value={values.email}
+                      onBlur={handleBlur}
+                      isRequired
+                      isInvalid={!!errors.email && touched.email}
                     />
+                    <FormErrorMessage fontSize={"0.73em"}>{errors.email}</FormErrorMessage>
                   </FormControl>
                   <FormControl
                     id="password"
                     name="password"
                     onChange={handleChange}
                     value={values.password}
+                    onBlur={handleBlur}
                     isRequired
+                    isInvalid={!!errors.password && touched.password}
                   >
-                    <FormLabel>Password</FormLabel>
+                    <FormLabel>Password</FormLabel >
                     <InputGroup>
                       <Input type={showPassword ? "text" : "password"} />
                       <InputRightElement h={"full"}>
@@ -142,6 +184,7 @@ export default function SignUpForm() {
                         </Button>
                       </InputRightElement>
                     </InputGroup>
+                    <FormErrorMessage fontSize={"0.73em"}>{errors.password}</FormErrorMessage>
                   </FormControl>
                   <Button
                     loadingText="Submitting"
