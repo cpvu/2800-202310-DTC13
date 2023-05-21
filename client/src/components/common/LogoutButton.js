e67acd372f50b92f48d9c07aff65ef57d3a5be90
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 16:13:34 -0700

    Add logout toast on load and success

--- client/src/components/common/LogoutButton.js
+++ client/src/components/common/LogoutButton.js
@@ -1,31 +1,49 @@
-import { Button } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { useSession, signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
+  const toast = useToast();
 
-    async function handleLogout(event) {
-        signOut({redirect: false});
+  async function handleLogout(event) {
+    signOut({ redirect: false });
 
-        const options = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: {}
-          };
+    toast({
+      title: "Logging out",
+      position: "top",
+      status: "loading",
+      duration: 1000,
+      isClosable: true,
+    });
 
-          const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-    
-          try {
-            let response = await fetch(baseURL + "/api/logout", options);    
-          } catch (err) {
-            console.log(err);
-          }
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {}
+    };
 
+    const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+    try {
+      let response = await fetch(baseURL + "/api/logout", options);
+    } catch (err) {
+      console.log(err);
     }
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return ( 
-        <>  {status == "authenticated" ?  <Button width={"80px"} onClick={(event) => {handleLogout(event)}}>Logout</Button> : null}</>
-    )
+    toast({
+      title: "Successfully logged out",
+      position: "top",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+
+  }
+
+  return (
+    <>  {status == "authenticated" ? <Button width={"80px"} onClick={(event) => { handleLogout(event) }}>Logout</Button> : null}</>
+  )
 }
