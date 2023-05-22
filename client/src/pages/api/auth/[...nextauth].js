a7d90eb1b3c0b6c5cbda0944641106297af88a67
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 19:32:38 -0700

    Refactor next auth to set express cookie

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -2,42 +2,48 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from "next-auth/providers/credentials"
 import { LOGIN_ENDPOINT } from '@/constants/endpoints';
 
-const options = {
-  secret: process.env.SECRET,
-  csrf: false,
-  providers: [
-    CredentialsProvider({
-      // Credentials provider options
-      async authorize(credentials) {
+const nextAuthOptions = (req, res) => {
+  return {
 
-        const payload = {
-          username: credentials.username,
-          password: credentials.password
-        }
-        const options = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        };
+    secret: process.env.SECRET,
+    csrf: false,
+    providers: [
+      CredentialsProvider({
+        // Credentials provider options
+        async authorize(credentials) {
 
-        const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+          const payload = {
+            username: credentials.username,
+            password: credentials.password
+          }
+          const options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(payload),
+          };
 
+          const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-        let response = await fetch(baseURL + LOGIN_ENDPOINT, options);
-        let responseJSON = await response.json();
+          let response = await fetch(baseURL + LOGIN_ENDPOINT, options);
+          let responseJSON = await response.json();
 
-        if (responseJSON.authenticated) {
-          return { id: 1, name: credentials.username };
-        } else {
-          throw new Error(responseJSON.message)
-        }
-      },
-    }),
-  ],
+          if (responseJSON.authenticated) {
+            const customCookie = response.headers.get("set-cookie")
+            res.setHeader('Set-Cookie', [customCookie])
+
+            return { id: 1, name: credentials.username };
+          } else {
+            throw new Error(responseJSON.message)
+          }
+        },
+      }),
+    ],
+  }
 
   // Other NextAuth.js options
 };
 
-export default (req, res) => NextAuth(req, res, options);
+export default (req, res) => {return NextAuth(req, res, nextAuthOptions(req, res))};
