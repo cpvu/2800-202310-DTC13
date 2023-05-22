37b2980f1432dce29d2e637090e4344acd4ab02f
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 19:35:12 -0700

    Add watchlist controller functions

---
+++ server/src/controllers/watchlistController.js
@@ -0,0 +1,15 @@
+export const getWatchlist = (req, res) => {
+
+}
+
+export const postAddWatchlist = (req, res) => {
+
+}
+
+export const postRemoveWatchlist = (req, res) => {
+
+}
+
+export const postUpdateWatchlist = (req, res) => {
+    
+}
