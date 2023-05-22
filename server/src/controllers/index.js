97f7f048045f131778f8c5fcc1fdc9a964edd60c
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-21 19:34:56 -0700

    Add watchlist route imports

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,4 +1,5 @@
 import { postLogin, postSignup, postLogout, postSendResetPasswordEmail, postResetPassword } from "./authController.js";
 import { postCoinDescription, postAskQuestion } from "./openController.js";
+import { postAddWatchlist, postRemoveWatchlist, postUpdateWatchlist, getWatchlist } from "./watchlistController.js";
 
-export { postLogin, postLogout, postResetPassword, postSignup, postSendResetPasswordEmail, postAskQuestion, postCoinDescription }
+export { postLogin, postLogout, postResetPassword, postSignup, postSendResetPasswordEmail, postAskQuestion, postCoinDescription, postAddWatchlist, postRemoveWatchlist, postUpdateWatchlist, getWatchlist }
