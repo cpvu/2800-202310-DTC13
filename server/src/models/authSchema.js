1b1920c6aac33851f6c9ab685b31807b32203ae1
yuyue-creater <michaelyuyue2012@gmail.com>
2023-05-21 17:24:59 -0700

    made a merge

--- server/src/models/authSchema.js
+++ server/src/models/authSchema.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   resetToken: String,
+  records: Array,
 });
 
 export const User = mongoose.model("User", userSchema);
