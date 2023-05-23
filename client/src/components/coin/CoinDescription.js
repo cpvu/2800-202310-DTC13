b68796085814548eec752228e565aed765aec495
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-22 22:30:21 -0700

    Add watchlist button

--- client/src/components/coin/CoinDescription.js
+++ client/src/components/coin/CoinDescription.js
@@ -1,50 +1,64 @@
 import React, { useEffect, useState } from "react";
-import { Box, Heading, Flex, HStack, Text, Card, CardHeader, CardBody, CardFooter, Stack, StackDivider } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Flex,
+  HStack,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  StackDivider,
+  Button,
+} from "@chakra-ui/react";
 import fetchCoinInformation from "./services/fetchCoinInformation";
 
 export default function CoinDescription({ description }) {
   return (
     <Box width={"100%"}>
-
       <Card>
         <CardBody>
-          <Stack divider={<StackDivider />} spacing='4'>
-          <Box>
-              <Heading size='xs' textTransform='uppercase'>
+          <Flex justifyContent={"right"}>
+            <Button>Add to Watchlist</Button>
+          </Flex>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
                 Market Cap:
               </Heading>
-              <Text pt='2' fontSize='sm'>
-              {description.marketCap}
+              <Text pt="2" fontSize="sm">
+                {description.marketCap}
               </Text>
             </Box>
             <Box>
-              <Heading size='xs' textTransform='uppercase'>
+              <Heading size="xs" textTransform="uppercase">
                 History:
               </Heading>
-              <Text pt='2' fontSize='sm'>
+              <Text pt="2" fontSize="sm">
                 {description.history}
               </Text>
             </Box>
             <Box>
-              <Heading size='xs' textTransform='uppercase'>
+              <Heading size="xs" textTransform="uppercase">
                 Founder
               </Heading>
-              <Text pt='2' fontSize='sm'>
+              <Text pt="2" fontSize="sm">
                 {description.founder}
               </Text>
             </Box>
             <Box>
-              <Heading size='xs' textTransform='uppercase'>
+              <Heading size="xs" textTransform="uppercase">
                 Year Founded
               </Heading>
-              <Text pt='2' fontSize='sm'>
-              {description.yearFounded}
+              <Text pt="2" fontSize="sm">
+                {description.yearFounded}
               </Text>
             </Box>
           </Stack>
         </CardBody>
       </Card>
-
     </Box>
   );
-};
+}
