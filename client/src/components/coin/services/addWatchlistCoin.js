66eb6da880a1f14c34a0d21756456691fd759e61
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-22 22:30:32 -0700

    Add watch list add api call

---
+++ client/src/components/coin/services/addWatchlistCoin.js
@@ -0,0 +1,13 @@
+export default function addWatchlistCoin() {
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      credentials: "include",
+    };
+  } catch (e) {
+    console.log(e);
+  }
+}
