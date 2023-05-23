0b1da80161f4daa1dd7273f842814e55bb3670d7
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-05-22 22:31:04 -0700

    Reformat component

--- client/src/components/coin/CoinPrice.js
+++ client/src/components/coin/CoinPrice.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 import fetchCoinPrice from "./services/fetchCoinPrice";
 import { useRouter } from "next/router";
 
-export default function CoinPrice({ price }) {
-    return (
-        <Box>
-            <Heading size={{ xs: "lg", lg: "2xl" }} >{price}</Heading>
-        </Box>
-    )
+export default function CoinPrice({ price, color }) {
+  return (
+    <Box>
+      <Heading color={color} mx={"5px"} size={{ xs: "md", lg: "2xl" }}>
+        {price}
+      </Heading>
+    </Box>
+  );
 }
