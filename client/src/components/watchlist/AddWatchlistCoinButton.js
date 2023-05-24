be6d2b652274280d28b26e23f332f18cc433c117
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-24 16:45:37 -0700

    Add logic to watchlist add

--- client/src/components/watchlist/AddWatchlistCoinButton.js
+++ client/src/components/watchlist/AddWatchlistCoinButton.js
@@ -1,7 +1,32 @@
 import { Button } from "@chakra-ui/react"
+import addWatchlistCoin from "../coin/services/addWatchlistCoin"
+import { useRouter } from "next/router";
+
+
 
 export default function AddWatchlistCoinButton() {
+    const router = useRouter();
+    
+    async function handleClick() {
+
+        const {coin, symbol} = router.query;
+
+        const coinData = {
+            name: coin.toString(),
+            symbol: symbol,
+            amount: 0,
+            averagePrice: 0,
+        }
+
+        await addWatchlistCoin(coinData);
+
+        console.log("Added");
+        return;
+    
+
+    }
+
     return(
-        <Button my={"10px"}> Add Watchlist</Button>
+        <Button my={"10px"} onClick={() => handleClick()}> Add Watchlist</Button>
     )
 }
