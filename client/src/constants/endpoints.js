89e17830a2191fac4a842ef58a8a1adcc9d46952
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-24 16:45:44 -0700

    Add watchlist endpoint

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,7 +1,12 @@
+//Auth endpoints
 export const FORGOT_PASSWORD_ENDPOINT = "/api/forgotPassword";
 export const RESET_PASSWORD_ENDPOINT = "/api/resetPassword/";
 export const SIGN_UP_ENDPOINT = "/api/signup/";
 export const LOGIN_ENDPOINT = "/api/login/";
 export const FAQ_ENDPOINT = "/api/askQuestion";
 
+//Open AI endpoints
 export const COIN_INFORMATION = "/api/coinDescription"
+
+//Watch list endpoints
+export const ADD_WATCHLIST_COIN = "/api/addWatchlist"
