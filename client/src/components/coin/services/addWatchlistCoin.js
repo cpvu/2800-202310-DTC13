24743c463729e340cf629fac6b0313437a2e6aaf
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-24 16:45:29 -0700

    Implement add watchlist item api call

--- client/src/components/coin/services/addWatchlistCoin.js
+++ client/src/components/coin/services/addWatchlistCoin.js
@@ -1,12 +1,31 @@
-export default function addWatchlistCoin() {
+import { ADD_WATCHLIST_COIN } from "@/constants/endpoints";
+
+export default async function addWatchlistCoin(userCoinData) {
   try {
+    const {name, symbol, amount, averagePrice} = userCoinData;
+
+    const payload = {
+      name: name,
+      symbol: symbol,
+      amount: amount,
+      averagePrice: averagePrice
+    }
+
     const options = {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
+      body: JSON.stringify(payload),
       credentials: "include",
     };
+
+    const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const endpoint = ADD_WATCHLIST_COIN;
+
+    const response = await fetch(baseURL + endpoint, options)
+    console.log(response)
+
   } catch (e) {
     console.log(e);
   }
