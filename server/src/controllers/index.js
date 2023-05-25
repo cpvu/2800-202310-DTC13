7c8cca0754a70240ca357a482becf32b0a7182b8
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-24 23:41:55 -0700

    Add article sentiment route

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,5 +1,5 @@
 import { postLogin, postSignup, postLogout, postSendResetPasswordEmail, postResetPassword } from "./authController.js";
-import { postCoinDescription, postAskQuestion } from "./openController.js";
+import { postCoinDescription, postAskQuestion, postArticleSentiment } from "./openController.js";
 import { postAddWatchlist, postRemoveWatchlist, postUpdateWatchlist, getWatchlist } from "./watchlistController.js";
 
-export { postLogin, postLogout, postResetPassword, postSignup, postSendResetPasswordEmail, postAskQuestion, postCoinDescription, postAddWatchlist, postRemoveWatchlist, postUpdateWatchlist, getWatchlist }
+export { postLogin, postLogout, postResetPassword, postSignup, postSendResetPasswordEmail, postAskQuestion, postCoinDescription, postAddWatchlist, postRemoveWatchlist, postUpdateWatchlist, getWatchlist, postArticleSentiment }
