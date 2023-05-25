707d2e629fdeb3dc7e4657e3c7dce26abddf1717
CPV-2021 <90016338+CPV-2021@users.noreply.github.com>
2023-05-24 20:59:55 -0700

    Complete display news articles

--- client/src/components/coin/CoinNews.js
+++ client/src/components/coin/CoinNews.js
@@ -1,9 +1,82 @@
-export default function CoinNews({ news }) {
-    return (
-        <>
-           {news ? news.map((newsItem, index) => (
-            <p key={index}>{newsItem.title}</p>
-          )) : <></>}
-        </>
-    )
-}
+import { useEffect } from 'react';
+import Slider from 'react-slick';
+import { Box, Text, Image, Flex, Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const NewsComponent = ({ news }) => {
+
+  useEffect(() => {
+    console.log(news)
+  }, [])
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+
+
+  const { coin } = useRouter().query;
+
+  const getDefaultImage = () => {
+    return `/${coin}.png`;
+  };
+
+  return (
+    <Box>
+      <Heading my={"30px"}>Latest News Sentiments</Heading>
+      <Slider {...settings}>
+        {news ? (
+          news.map((newsItem, index) => (
+            <Box key={index} p={4} textAlign="center" maxW={"500px"} maxH={"500px"}>
+              <Flex justifyContent={"center"}>
+                {newsItem.image_url ? (
+                  <Image src={newsItem.image_url} alt="News" align={"center"} w='80%' maxHeight='60%' />
+                ) : (
+                  <Image src={getDefaultImage()} alt="Default" maxWidth='56%' maxHeight='60%' my={"auto"} />
+                )}
+              </Flex>
+              <Text fontSize="lg" fontWeight="bold" mt={4}>
+                {newsItem.title}
+              </Text>
+              <Text fontSize={"12px"}>{newsItem.description ? (newsItem.description).split('. ')[0] : <></>}</Text>
+              <Text color="blue.500" textDecoration="underline" mt={2} as="a" href={newsItem.link} target="_blank" rel="noopener noreferrer">
+                Read More
+              </Text>
+            </Box>
+          ))
+        ) : (
+          <Text>No news available.</Text>
+        )}
+      </Slider>
+    </Box>
+  );
+};
+
+export default NewsComponent;
--- client/src/components/coin/services/fetchCoinNews.js
+++ client/src/components/coin/services/fetchCoinNews.js
@@ -7,7 +7,7 @@ export default async function fetchCoinNews(coin) {
         let response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_22499c45bb1fac4bfbd4adfb4a135e94affbe&qInTitle=${coin}&language=en`, options);
         let responseJSON = await response.json();
         
-        return responseJSON.results.slice(0,5);
+        return responseJSON.results;
     } catch (e) {
         console.log(e)
     }
